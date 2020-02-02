fa70f628f4ed52ad78edf4e39cbb0ffb38e0fa27
Lukáš Karas <lukaskaras.lk@gmail.com>
2020-02-02 12:03:09 +0100

    Remove test endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,6 @@ const router = express.Router()
 
 const itemController = new ItemController()
 
-router.get('/test', async (req, res) => {
-  console.log('called test')
-  res.sendStatus(200)
-})
-
 router.get('/item', itemController.getItem)
 router.post('/item', itemController.postItem)
 router.delete('/item', itemController.deleteItem)
