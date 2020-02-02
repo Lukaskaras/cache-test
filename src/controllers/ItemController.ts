711dbabc3879046d001a774fb470000057aae6fd
Lukáš Karas <lukaskaras.lk@gmail.com>
2020-02-02 11:50:17 +0100

    refactor controller

--- src/controllers/ItemController.ts
+++ src/controllers/ItemController.ts
@@ -8,107 +8,114 @@ import { generateRandomString } from '../utils'
 const defaultTtl = config.get('defaultTtl')
 const maxItems = config.get('maxItems')
 
-export const postItem = async (req: Request, res: Response) => {
-  const { key, value, ttl } = req.body
-
-  const itemTtl = ttl || defaultTtl
-  const expiresAt = moment().add(itemTtl, 'seconds').toDate()
-  await createItem(key, value, expiresAt)
-
-  res.sendStatus(200)
-}
-
-export const getAllItems = async (req: Request, res: Response): Promise<void> => {
-  const items = await ItemModel.find({ expiresAt: { $gte: new Date() }}).exec()
-  res.send(items)
-}
-
-export const getItem = async (req: Request, res: Response): Promise<void> => {
-  const { key } = req.query
-  const item = await ItemModel.findOne({ key }).exec()
-
-  const now = new Date()
-  const expiresAt = moment().add(defaultTtl, 'seconds').toDate()
-  const randomString = generateRandomString()
-
-  let updatedItem: Item
-  if (!item) {
-    await createItem(key, randomString, expiresAt)
-    updatedItem = {
-      key,
-      value: randomString,
-      expiresAt
-    }
+export default class ItemController {
+  constructor() {
+    this.postItem = this.postItem.bind(this)
+    this.getItem = this.getItem.bind(this)
   }
 
-  if (item) {
-    if (new Date(item.expiresAt).getTime() < now.getTime()) {
-      console.log('item is expired')
+  public async postItem (req: Request, res: Response) {
+    const { key, value, ttl } = req.body
+
+    const itemTtl = ttl || defaultTtl
+    const expiresAt = moment().add(itemTtl, 'seconds').toDate()
+    await this.createItem(key, value, expiresAt)
+
+    res.sendStatus(200)
+  }
+
+  public async getAllItems(req: Request, res: Response): Promise<void> {
+    const items = await ItemModel.find({ expiresAt: { $gte: new Date() }}).exec()
+    res.send(items)
+  }
+
+  public async getItem (req: Request, res: Response): Promise<void> {
+    const { key } = req.query
+    const item = await ItemModel.findOne({ key }).exec()
+
+    const now = new Date()
+    const expiresAt = moment().add(defaultTtl, 'seconds').toDate()
+    const randomString = generateRandomString()
+
+    let updatedItem: Item
+    if (!item) {
+      await this.createItem(key, randomString, expiresAt)
       updatedItem = {
         key,
         value: randomString,
         expiresAt
       }
-      res.send(updatedItem)
-      await ItemModel.updateOne({ key }, updatedItem)
-    } else {
-      console.log('Cache hit')
-      updatedItem = {
-        key,
-        value: item.value,
-        expiresAt
-      }
-      await ItemModel.updateOne({ key }, updatedItem)
     }
+
+    if (item) {
+      if (new Date(item.expiresAt).getTime() < now.getTime()) {
+        console.log('item is expired')
+        updatedItem = {
+          key,
+          value: randomString,
+          expiresAt
+        }
+        res.send(updatedItem)
+        await ItemModel.updateOne({ key }, updatedItem)
+      } else {
+        console.log('Cache hit')
+        updatedItem = {
+          key,
+          value: item.value,
+          expiresAt
+        }
+        await ItemModel.updateOne({ key }, updatedItem)
+      }
+    }
+
+    res.send(updatedItem)
   }
 
-  res.send(updatedItem)
-}
+  public async deleteItem (req: Request, res: Response) {
+    const { key } = req.query
 
-export const deleteItem = async (req: Request, res: Response) => {
-  const { key } = req.query
+    const result = await ItemModel.deleteOne({ key }).exec()
 
-  const result = await ItemModel.deleteOne({ key }).exec()
+    if (!result.deletedCount) {
+      res.sendStatus(404)
+      return
+    }
 
-  if (!result.deletedCount) {
-    res.sendStatus(404)
-    return
+    res.sendStatus(200)
   }
 
-  res.sendStatus(200)
-}
-
-export const deleteAllItems = async (req: Request, res: Response) => {
-  await ItemModel.deleteMany({}).exec()
-  res.sendStatus(200)
-}
-
-const createItem = async (key: string, value: string, expiresAt: Date): Promise<void> => {
-  const allItems = await ItemModel.find({}).sort({ expiresAt: 'asc' }).exec()
-
-  // when limit is reached, item with oldest expiration is updated
-  if (allItems.length === maxItems) {
-    await ItemModel.updateOne({ _id: allItems[0]._id }, {
-      key,
-      value,
-      expiresAt
-    })
-    return
+  public async deleteAllItems (req: Request, res: Response) {
+    await ItemModel.deleteMany({}).exec()
+    res.sendStatus(200)
   }
 
-  try {
-    await ItemModel.create({
-      key,
-      value,
-      expiresAt
-    })
-  } catch (err) {
-    if (err.code === mongoErrorCodes.duplicateKey) {
-      console.log('item already exists, going to update')
-      await ItemModel.updateOne({ key }, {
+  private async createItem (key: string, value: string, expiresAt: Date): Promise<void> {
+    const allItems = await ItemModel.find({}).sort({ expiresAt: 'asc' }).exec()
+
+    // when limit is reached, item with oldest expiration is updated
+    if (allItems.length === maxItems) {
+      await ItemModel.updateOne({ _id: allItems[0]._id }, {
+        key,
         value,
         expiresAt
       })
+      return
+    }
+
+    try {
+      await ItemModel.create({
+        key,
+        value,
+        expiresAt
+      })
+    } catch (err) {
+      if (err.code === mongoErrorCodes.duplicateKey) {
+        console.log('item already exists, going to update')
+        await ItemModel.updateOne({ key }, {
+          value,
+          expiresAt
+        })
+      }
     }
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,18 @@
 import express from 'express'
-import { deleteAllItems, deleteItem, getAllItems, getItem, postItem } from '../controllers/ItemController'
+import ItemController from '../controllers/ItemController'
 const router = express.Router()
+
+const itemController = new ItemController()
 
 router.get('/test', async (req, res) => {
   console.log('called test')
   res.sendStatus(200)
 })
 
-router.get('/item', getItem)
-router.post('/item', postItem)
-router.delete('/item', deleteItem)
-router.get('/items', getAllItems)
-router.delete('/items', deleteAllItems)
+router.get('/item', itemController.getItem)
+router.post('/item', itemController.postItem)
+router.delete('/item', itemController.deleteItem)
+router.get('/items', itemController.getAllItems)
+router.delete('/items', itemController.deleteAllItems)
 
 export default router
